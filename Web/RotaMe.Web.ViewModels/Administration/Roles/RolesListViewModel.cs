acaf1223c2757a2e3343c3d9b691f48ee661529e
vankatalp360 <ivan.dimitrov81@gmail.com>
2020-02-01 16:48:31 +0200

    Add List all roles page

---
+++ Web/RotaMe.Web.ViewModels/Administration/Roles/RolesListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RotaMe.Web.ViewModels.Administration.Roles
+{
+    public class RolesListViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int UsersCount { get; set; }
+
+        public string CardIcon { get; set; }
+    }
+}
