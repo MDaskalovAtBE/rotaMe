533f9686498ecea2d645f3b85c5cb5de9847e696
vankatalp360 <ivan.dimitrov81@gmail.com>
2020-03-24 16:57:49 +0200

    Add Create EventNeed page and List all event needs

---
+++ Services/RotaMe.Sevices.Models/Owner/Events/EventNeedCreateServiceModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RotaMe.Sevices.Models.Owner.Events
+{
+    public class EventNeedCreateServiceModel
+    {
+        public int EventId { get; set; }
+
+        public string Date { get; set; }
+
+        public int MinimalUsers { get; set; }
+
+        public int MaximumUsers { get; set; }
+    }
+}
